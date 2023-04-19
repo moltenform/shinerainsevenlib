75f869d03c92d89b5d5bfd70f843b0d5b085695a
moltenform.com(Ben Fisher) <amoltenform@gmail.com>
2023-04-18 19:55:13 -0700

    work on gen_test

--- tests/infrastructure/test_gen_tests_divide_functions.py
+++ tests/infrastructure/test_gen_tests_divide_functions.py
@@ -455,64 +455,83 @@ class RunTestByString(RunTestByStringBase):
         return result
         
     def runAndRecordResults(self, listTests, defaultTestsIfEmpty=None):
+        for i, testSections in enumerate(listTests):
+            try:
+                self.runAndRecordResultsOne(defaultTestsIfEmpty, testSections)
+            except Exception as e:
+                trace('exception in test ', testSections.get('testName', ''), self.context)
+                raise
+        
+        updatedTests = self.unparseString(listTests)
+        print(updatedTests)
+
+    def runAllTests(self):
         results = []
         for i, testSections in enumerate(listTests):
-            prep = self.prepOne(testSections)
+            try:
+                self.runAllTestsOne(testSections)
+            except Exception as e:
+                trace('exception in test ', testSections.get('testName', ''), self.context)
+                raise
+        
+        trace('tests complete', self.context)
+
+    def runAndRecordResultsOne(self, defaultTestsIfEmpty, testSections):
+        prep = self.prepOne(testSections)
+        hadException = None
+        try:
+            got = self.runOne(testSections, prep)
+        except Exception as e:
+            hadException = e
+        
+        if hadException:
+            # build a completely new section
+            self.deleteOtherSections(testSections, exceptionSeen=True)
+            
+            if not (testSections.get('expect error type').strip() and isinstance(hadException, self.evalStr(testSections['expect error type']))):
+                testSections['expect error type'] = hadException.__class__.__name__
+            
+            if not (testSections.get('expect error contains').strip() and testSections['expect error contains'] in str(hadException)):
+                testSections['expect error contains'] = str(hadException)
+        else:
+            self.deleteOtherSections(testSections, exceptionSeen=False)
+            
+            got = self.interpretOne(got)
+            if defaultTestsIfEmpty and len(testSections) <= 2:
+                for sectionName in defaultTestsIfEmpty:
+                    testSections[sectionName] = ''
+            
+            for sectionName in testSections:
+                if sectionName.startswith('expect '):
+                    sToEval = sectionName.split('`')[1]
+                    wasEvalled = self.evalStr(wasEvalled, got)
+                    testSections[sectionName] = wasEvalled
+    
+            
+    def runAllTestsOne(self, testSections):
+        prep = self.prepOne(testSections)
+        if any(sectionName.startswith('expect error' for sectionName in testSections)):
             hadException = None
             try:
                 got = self.runOne(testSections, prep)
             except Exception as e:
                 hadException = e
             
-            if hadException:
-                # build a completely new section
-                self.deleteOtherSections(testSections, exceptionSeen=True)
-                
-                if not (testSections.get('expect error type').strip() and isinstance(hadException, self.evalStr(testSections['expect error type']))):
-                    testSections['expect error type'] = hadException.__class__.__name__
-                
-                if not (testSections.get('expect error contains').strip() and testSections['expect error contains'] in str(hadException)):
-                    testSections['expect error contains'] = str(hadException)
-            else:
-                self.deleteOtherSections(testSections, exceptionSeen=False)
-                
-                got = self.interpretOne(got)
-                if defaultTestsIfEmpty and len(testSections) <= 2:
-                    for sectionName in defaultTestsIfEmpty:
-                        testSections[sectionName] = ''
-                
-                for sectionName in testSections:
-                    if sectionName.startswith('expect '):
-                        sToEval = sectionName.split('`')[1]
-                        wasEvalled = self.evalStr(wasEvalled, got)
-                        testSections[sectionName] = wasEvalled
-    
-    def runAllTests(self):
-        results = []
-        for i, testSections in enumerate(listTests):
-            prep = self.prepOne(testSections)
-            if any(sectionName.startswith('expect error' for sectionName in testSections)):
-                hadException = None
-                try:
-                    got = self.runOne(testSections, prep)
-                except Exception as e:
-                    hadException = e
-                
-                assertTrue(hadException, 'exception expected but did not occur')
-                if 'expect error type' in testSections:
-                    assertTrue(isinstance(hadException, self.evalStr(testSections['expect error type'])),
-                        f"wrong error type expected {testSections['expect error type']} but got {hadException.__class__.__name__}")
-                if 'expect error contains' in testSections:
-                    assertTrue(testSections['expect error contains'] in str(hadException),
-                        f"string '{testSections['expect error contains']}' not in '{str(e)}'")
-            else:
-                got = self.runOne(testSections, prep)
-                got = self.interpretOne(got)
-                for sectionName in testSections:
-                    if sectionName.startswith('expect '):
-                        sToEval = sectionName.split('`')[1]
-                        wasEvalled = self.evalStr(wasEvalled, got)
-                        assertEq(testSections[sectionName], wasEvalled, sectionName)
+            assertTrue(hadException, 'exception expected but did not occur', testSections)
+            if 'expect error type' in testSections:
+                assertTrue(isinstance(hadException, self.evalStr(testSections['expect error type'])),
+                    f"wrong error type expected {testSections['expect error type']} but got {hadException.__class__.__name__}")
+            if 'expect error contains' in testSections:
+                assertTrue(testSections['expect error contains'] in str(hadException),
+                    f"string '{testSections['expect error contains']}' not in '{str(e)}'")
+        else:
+            got = self.runOne(testSections, prep)
+            got = self.interpretOne(got)
+            for sectionName in testSections:
+                if sectionName.startswith('expect '):
+                    sToEval = sectionName.split('`')[1]
+                    wasEvalled = self.evalStr(wasEvalled, got)
+                    assertEq(testSections[sectionName], wasEvalled, sectionName)
         
     
     def deleteOtherSections(self, exceptionSeen):
@@ -525,6 +544,7 @@ class RunTestByString(RunTestByStringBase):
                     del testSections[key]
     
     def evalStr(self, s, got=None):
+        # the local var `got` will be magically passed in.
         return eval(s)
     
     ######################
@@ -554,24 +574,46 @@ class RunTestByString(RunTestByStringBase):
         got.strMapTestNameToSection = strExpected
         return got
 
+defaultSubstituteChars = [['\0', '0x00']]
+def directoryToString(dir, substituteChars=defaultSubstituteChars):
+    # all leading and trailing whitespace is lost
+    result = 'FILES'
+    for f, short in files.recurseFiles(dir):
+        result += '\n' + f.replace(dir, '').replace(files.dirSep, '/')
+        contents = files.readAll(f)
+        indentedContents = '    ' + contents.replace('\n', '    \n')
+        result += '\n' + indentedContents
+    
+    if substituteChars:
+        for binary, text in substituteChars:
+            result = result.replace(binary, text)
+    
+    return result
 
-def a():
-    return 'a'+b()
-def b():
-    return 'b'+c()
-def c():
-    return 'c'+d()
-def d():
-    raise RuntimeError()
+def _deleteDirectoryAndReplaceContentsByStringParse(s, substituteChars=defaultSubstituteChars):
+    if substituteChars:
+        for binary, text in substituteChars:
+            s = s.replace(text, binary)
+    
+    results = []
+    assertTrue(s.startswith('FILES'))
+    s = s[len('FILES'):]
+    s = s.lstrip()
+    sections = re.split(r'\n\w', s)
+    for section in sections:
+        fileName = section.split('\n')[0]
+        lines = section.split('\n')[1:]
+        lines = [line.strip() for line in lines]
+        fileContents = '\n'.join(lines)
+        results.append(Bucket(fileName=fileName, fileContents=fileContents))
+    
+    return results
+        
+def deleteDirectoryAndReplaceContentsByString(s, dir, substituteChars=defaultSubstituteChars):
+    files.ensureEmptyDir(dir)
+    for result in _deleteDirectoryAndReplaceContentsByStringParse(s, substituteChars=substituteChars):
+        files.writeAll(result.fileName, result.fileContents)
+        
+    
 
-def testEval():
-    try:
-        a()
-    except Exception as e:
-        trace('got a ' + e.__class__.__name__)
-        raise
-    #~ myVar = 'abc'
-    #~ tryString = '''Bucket(bar=testCases[4]).bar + myVar'''
-    #~ trace(eval(tryString))
 
-testEval()
