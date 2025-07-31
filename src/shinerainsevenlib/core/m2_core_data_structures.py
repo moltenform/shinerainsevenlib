4447881d33c3fbd3043000a4bfce6d43251a174e
moltenform.com(Ben Fisher) <amoltenform@gmail.com>
2025-07-31 09:21:22 -0700

    Add toml file

---
+++ src/shinerainsevenlib/core/m2_core_data_structures.py
@@ -0,0 +1,11 @@
+
+# shinerainsevenlib (Ben Fisher, moltenform.com)
+# Released under the LGPLv2 License
+
+class Default:
+    "Use with function parameters to see if a value was passed, or left to default"
+    def __repr__( self ):
+        return "DefaultValue"
+
+DefaultValue = Default()
+
